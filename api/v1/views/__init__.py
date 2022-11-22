65cbecef5e2ef7f00eada6018267529c5e8dbae2
Nworah-Gabrielclear <gabrielnworah6@gmail.com>
2022-11-22 02:24:50 +0100

    My task commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,6 @@
 """ Blueprint for API """
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
@@ -12,13 +11,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-"""
-if app_views is not None:
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
-    from api.v1.views.places_amenities import * """
+from api.v1.views.places_amenities import *
